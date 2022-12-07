48328bbe83e80f9d39705bad1a6d25c7024738ec
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2022-12-06 22:24:17 -0500

    followed along with the guided project video to practice writing functions that create a new component for each item in a list of data

--- src/index.js
+++ src/index.js
@@ -7,37 +7,54 @@ const imageData = [
   { imageURL: 'https://images.dog.ceo/breeds/mastiff-bull/n02108422_2947.jpg' },
 ]
 
+function makeImage(imgURL) {
+  const image = document.createElement('img')
+  image.src = imgURL.imageURL;
+  image.style.height = '10em';
+  image.classList.add('doggo');
+  return image;
+}
+
+imageData.forEach(imgObj => {
+  const imgElement = makeImage(imgObj);
+  // document.body.prepend(imgElement);
+})
 
 // TASK 1- Import the data we need to "hydrate" our component.
 //  On the one hand, the default export from data/panelData.js
 //  On the other hand, the default export from data/constants.js
 //  Destructure `open` and `close` from the constants
+import panelData from './data/panelData';
+import linkData from './data/linkData';
+import constants from './data/constants';
 
+const { open, close } = constants;
 
 // TASK 2- Verify our imports using log statements
-console.log() // log the panelData
-console.log() // log the open arrow
-console.log() // log the close arrow
+console.log(panelData) // log the panelData
+console.log(linkData) // log the linkData
+console.log(open) // log the open arrow
+console.log(close) // log the close arrow
 
 
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
 //  We will generate the panel with code, and we'll need the parent
 //  so we can append the code-generated panel to the DOM.
-const accordion = null
+const accordion = document.querySelector('.accordion');
 
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
-function makePanel(/* what data does the panel need? */) {
+function makePanel({ title, content }) {
 
 
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
+  const panel = document.createElement('div');
+  const panelBar = document.createElement('div');
+  const panelContent = document.createElement('div');
+  const panelTitle = document.createElement('h3');
+  const panelButtons = document.createElement('div');
+  const openButton = document.createElement('button');
+  const closeButton = document.createElement('button');
 
 
   // TASK 6- Setup the structure of our elements
@@ -53,34 +70,84 @@ function makePanel(/* what data does the panel need? */) {
       <div></div>           // panelContent
     </div>
   */
+  panel.appendChild(panelBar);
+  panel.appendChild(panelContent);
+  panelBar.appendChild(panelTitle);
+  panelBar.appendChild(panelButtons);
+  panelButtons.appendChild(openButton);
+  panelButtons.appendChild(closeButton);
 
 
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
-
+  panel.classList.add('panel');
+  panelBar.classList.add('panel-bar');
+  panelButtons.classList.add('panel-buttons');
+  openButton.classList.add('panel-btn-open');
+  closeButton.classList.add('panel-btn-close', 'hide-btn');
+  panelContent.classList.add('panel-content');
 
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
-
+  panelTitle.textContent = title;
+  panelContent.textContent = content;
+  openButton.textContent = open;
+  closeButton.textContent = close;
 
   // TASK 9- When the 'open' or 'close' buttons are clicked, the content is toggled on/off:
   //  - the open button needs to go away (the 'hide-btn' class name controls this)
   //  - the close button needs to show (the 'hide-btn' class name controls this)
   //  - the contents need to show (the 'toggle-on' class name controls this)
-
+  panelButtons.addEventListener('click', () => {
+    // 1. hide open button => 2nd click show open button
+    // 2. show close button => 2nd click hide close button
+    // 3. show panel content => 2nd click hide panel content
+    openButton.classList.toggle('hide-btn');
+    closeButton.classList.toggle('hide-btn');
+    panelContent.classList.toggle('toggle-on');
+  })
 
   // don't forget to return the panel!
-  return null
+  return panel;
 }
-
 
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
 //  We can do this with a single forEach, or with a map and a forEach.
+const panelElements = panelData.map(panelObj => {
+  return makePanel(panelObj);
+})
+console.log(panelElements);
 
+panelElements.forEach(panelElement => {
+  accordion.appendChild(panelElement);
+})
+
+// panelData.forEach(panelObj => {
+//   let panelElement = makePanel(panelObj);
+//   accordion.appendChild(panelElement);
+// })
 
 // [STRETCH] Comment out the links inside the nav and
 // write a linkMaker that takes { href, className, text }
 // and returns an anchor tag with the right href, class and textContent.
 // Loop over the 'linkData' in the data folder, generate anchor tags
 // and append them to the nav.
+function linkMaker({ href, className, text }) {
+  const link = document.createElement('a');
+  link.href = href;
+  link.className = className;
+  link.textContent = text;
+
+  return link;
+}
+
+linkData.forEach(linkObj => {
+  const linkElement = linkMaker(linkObj);
+  document.querySelector('nav').appendChild(linkElement);
+})
+
+// <a href="#" class="nav-item">Home</a>
+// <a href="#" class="nav-item">About</a>
+// <a href="#" class="nav-item">Blog</a>
+// <a href="#" class="nav-item">Contact</a>
